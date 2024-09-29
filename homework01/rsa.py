8cc60683f9d061ae306219f37130a63f19630d3d
petlya13 <133315034+petlya13@users.noreply.github.com>
2024-09-29 23:19:45 +0300

    Add files via upload

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,11 +1,7 @@
 import random
-import typing as tp
 
-
-def is_prime(n: int) -> bool:
+def is_prime(n):
     """
-    Tests to see if a number is prime.
-
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -13,94 +9,71 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    dividers = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            dividers.append(i)
+            dividers.append(n//i)
+    if len(dividers) == 2:
+        return True
+    return False
+
+def gcd(a, b):
+    a, b = sorted([a, b], reverse=True)
+
+    while b:
+        a, b = b, a%b
+    return a
 
 
-def gcd(a: int, b: int) -> int:
+def multiplicative_inverse(e, phi):
     """
-    Euclid's algorithm for determining the greatest common divisor.
-
-    >>> gcd(12, 15)
-    3
-    >>> gcd(3, 7)
-    1
-    """
-    # PUT YOUR CODE HERE
-    pass
-
-
-def multiplicative_inverse(e: int, phi: int) -> int:
-    """
-    Euclid's extended algorithm for finding the multiplicative
-    inverse of two numbers.
-
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+
+    tablo = []
+    a, b = sorted([e, phi], reverse=True)
+
+    while a % b != 0:
+        tablo.append([a, b, a % b, a // b, None, None])
+        a, b = b, a % b
+
+    tablo[-1][-2], tablo[-1][-1] = 0, 1
 
 
-def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+    for i in range(len(tablo)-2, -1, -1):
+        prev_x, prev_y, a_div_b = tablo[i+1][-2], tablo[i+1][-1], tablo[i][3]
+        x = tablo[i+1][-1]
+        y =  prev_x - prev_y * a_div_b
+
+        tablo[i][-2], tablo[i][-1] = x, y
+
+    return tablo[0][-1]
+
+
+def generate_keypair(p, q):
+
     if not (is_prime(p) and is_prime(q)):
-        raise ValueError("Both numbers must be prime.")
+        raise ValueError('Both numbers must be prime.')
     elif p == q:
-        raise ValueError("p and q cannot be equal")
+        raise ValueError('p and q cannot be equal')
 
-    # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
-    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
-    # Use Extended Euclid's Algorithm to generate the private key
+
     d = multiplicative_inverse(e, phi)
 
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
 
 
-def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
-    # Unpack the key into it's components
-    key, n = pk
-    # Convert each letter in the plaintext to numbers based on
-    # the character using a^b mod m
-    cipher = [(ord(char) ** key) % n for char in plaintext]
-    # Return the array of bytes
-    return cipher
-
-
-def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
-    # Unpack the key into its components
-    key, n = pk
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
-    # Return the array of bytes as a string
-    return "".join(plain)
-
-
-if __name__ == "__main__":
-    print("RSA Encrypter/ Decrypter")
-    p = int(input("Enter a prime number (17, 19, 23, etc): "))
-    q = int(input("Enter another prime number (Not one you entered above): "))
-    print("Generating your public/private keypairs now . . .")
-    public, private = generate_keypair(p, q)
-    print("Your public key is ", public, " and your private key is ", private)
-    message = input("Enter a message to encrypt with your private key: ")
-    encrypted_msg = encrypt(private, message)
-    print("Your encrypted message is: ")
-    print("".join(map(lambda x: str(x), encrypted_msg)))
-    print("Decrypting message with public key ", public, " . . .")
-    print("Your message is:")
-    print(decrypt(public, encrypted_msg))
+print(multiplicative_inverse(7, 40))
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,23 +1,58 @@
-def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+from string import ascii_lowercase, ascii_uppercase
+
+a_upper, z_upper = ord("A"), ord("Z")
+a_lower, z_lower = ord("a"), ord("z")
+
+def encrypt_vigenere(plaintext, keyword):
     """
-    Encrypts plaintext using a Vigenere cipher.
 
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
+
     >>> encrypt_vigenere("python", "a")
     'python'
+
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
+
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    global a_upper, a_lower, z_upper, z_lower
+
+    start_len = len(keyword)
+    pointer = 0
+    while len(keyword) < len(plaintext):
+        keyword += keyword[pointer % start_len]
+        pointer += 1
+
+    ciphertext = []
+    for symbol_index in range(len(plaintext)):
+
+        symbol = plaintext[symbol_index]
+
+        if symbol in ascii_uppercase:
+
+            step = ord(keyword[symbol_index]) - a_upper
+
+            sm_code = (ord(symbol) + step) % z_upper
+
+            symbol = chr(sm_code) if sm_code >= a_upper else chr(sm_code + a_upper - 1)
+
+        elif symbol in ascii_lowercase:
+
+            step = ord(keyword[symbol_index]) - a_lower
+            sm_code = (ord(symbol) + step) % z_lower
+            symbol = chr(sm_code) if sm_code >= a_lower else chr(sm_code + a_lower - 1)
+        ciphertext.append(symbol)
+
+    ciphertext = "".join(ciphertext)
+
     return ciphertext
 
 
-def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
+def decrypt_vigenere(ciphertext, keyword):
 
+    """
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -25,6 +60,32 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    # PUT YOUR CODE HERE
+
+    global a_upper, a_lower, z_upper, z_lower
+
+    start_len = len(keyword)
+    pointer = 0
+    while len(keyword) < len(ciphertext):
+        keyword += keyword[pointer % start_len]
+        pointer += 1
+
+    plaintext = []
+
+    for symbol_index in range(len(ciphertext)):
+
+        symbol = ciphertext[symbol_index]
+
+        if symbol in ascii_uppercase:
+            step = ord(keyword[symbol_index]) - a_upper
+            sm_code = (ord(symbol) - step)
+            symbol = chr(sm_code) if sm_code >= a_upper else chr(z_upper + 1 - (a_upper - sm_code))
+
+        elif symbol in ascii_lowercase:
+            step = ord(keyword[symbol_index]) - a_lower
+            sm_code = (ord(symbol) - step)
+            symbol = chr(sm_code) if sm_code >= a_lower else chr(z_lower + 1 - (a_lower - sm_code))
+        plaintext.append(symbol)
+
+    plaintext = "".join(plaintext)
+
     return plaintext
