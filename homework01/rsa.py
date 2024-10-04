f5fe895ad3f71603374e364eba0110752f3c203b
VTiganov <vadimyxy@gmail.com>
2024-10-04 23:33:44 +0300

    Реализована функция generate_keypair()

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -65,9 +65,11 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
 
     # n = pq
     # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
     # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
