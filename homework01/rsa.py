457ed4fff44ac150f0522bbb947437414f4f9937
VTiganov <vadimyxy@gmail.com>
2024-10-04 23:20:33 +0300

    Реализована функция is_prime()

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -14,7 +14,12 @@ def is_prime(n: int) -> bool:
     False
     """
     # PUT YOUR CODE HERE
-    pass 
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 def gcd(a: int, b: int) -> int:
